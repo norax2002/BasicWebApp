92bd158adeada626480c12584104a2b2d57ae00c
DESKTOP-AHUFP4E\user <bierignoah@gmail.com>
2022-07-15 11:11:58 +0200

    leer

--- src/main/java/de/tum/in/ase/eist/QueryProcessor.java
+++ src/main/java/de/tum/in/ase/eist/QueryProcessor.java
@@ -13,8 +13,19 @@ public class QueryProcessor {
                     "writer in the English language and the world's pre-eminent dramatist.";
         } else if (query.contains("name")) {
            return "MyTeam";
-        } else { // TODO extend the programm here
-            return "";
+        } else {
+
+
+
+
+
+
+
+
+
+
+
+            return "173";
         }
     }
 }
